3a609f35e4b32202aa00320e59c9364692ca5525
Md Zeeshan <91277213+Zeesshu1@users.noreply.github.com>
2025-12-12 00:18:14 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -16,6 +16,6 @@ contract MyToken is ERC20, Ownabl
         _mint(msg.sender, initialSupply);
     }
 
-    // आप बाद में भविष्य में टोकन को मिंट (Mint) करने के लिए यह फ़ंक्शन जोड़ सकते हैं।
+    // आप बाद में भविष्य में टोकन को मिंट (Mint) करने के लिए यह फ़ंक्शन जोड़ सकते ह
     // यह फ़ंक्शन केवल कॉन्ट्रैक्ट के मालिक (Owner) द्वारा कॉल किया जा सकता ह
     function mint(address to, uint256 amount) public onlyO
