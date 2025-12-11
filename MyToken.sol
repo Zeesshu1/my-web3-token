027514f81fc0a16c92ee30dbfeaa33c2dc0c9c1a
Md Zeeshan <91277213+Zeesshu1@users.noreply.github.com>
2025-12-12 00:27:15 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -8,7 +8,7 @@ contract MyToken is ERC20, Ownabl
     // Constructor में टोकन का नाम, सिंबल, और कुल सप्लाई (Initial Suppसेट करें।
     // यहाँ 1000 
     // ERC-20 में 18 डेसिमल होते हैं, इए 1000 *e का ग होता है।
-    constructor(uint256 init
+    constructor(uint2
         ERC20("My DeFi
         Ownab
     {
