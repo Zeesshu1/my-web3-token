9078fdb1f3cc6049801aca43b2480a96c5760434
Md Zeeshan <91277213+Zeesshu1@users.noreply.github.com>
2025-12-12 00:22:41 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -6,7 +6,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownabl
     // Constructor में टोकन का नाम, सिंबल, और कुल सप्लाई (Initial Suppसेट करें।
-    // यहाँ 1000 टोक
+    // यहाँ 1000 l
     // ERC-20 में 18 डेसिमल होते हैं, इसलिए 1000 *e का उपयोग होता है।
     constructor(uint256 initialSu
         ERC20("My DeFi Token", "MD
