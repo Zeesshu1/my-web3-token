8d63ebb58f56c2e695390a5c824867d52ce5b3da
Md Zeeshan <91277213+Zeesshu1@users.noreply.github.com>
2025-12-12 00:24:12 +0530

    Update MyToken.sol

--- MyToken.sol
+++ MyToken.sol
@@ -10,7 +10,7 @@ contract MyToken is ERC20, Ownabl
     // ERC-20 में 18 डेसिमल होते हैं, इसलिए 1000 *e का उपयोग होता है।
     constructor(uint256 init
         ERC20("My DeFi
-        Ownable(msg
+        Ownable(ms
     {
         // Constructor, टोकन बनाने वाले (Doyer) को initialSupply मिंट करता है।
         _mint(msg.sender, initialSu
